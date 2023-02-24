6ed87b155a31df48943b050218af2b61b5d7ab4e
p_tsanev <plamen_canev@abv.bg>
2023-02-24 21:58:22 +0200

    Working chat prototype

--- app/main/_rcc/_chat/Chat.tsx
+++ app/main/_rcc/_chat/Chat.tsx
@@ -1,9 +1,34 @@
+'use client';
+import { useState } from 'react';
 import styles from './chat.module.css';
 
 export default function Chat(){
+    const [inputVal, setInputVal] = useState('');
+    const [messages, setMessages] = useState<string[]>([]);
+
     return(
         <div className={styles.chat}>
-            chat
+            <div className={styles.chatBox}>
+                {messages.map((message, idx) => {
+                    return <div key={idx} className={styles.message}>{message}</div>
+                })}
+            </div>
+            <div className={styles.inputs}>
+                <div style={{width: '50px', height: '50px'}}>
+
+                </div>
+                <input type="text" placeholder='Type a message.....'
+                value={inputVal}
+                onChange={(event) => setInputVal(event.target.value)}
+                onKeyDown={(event) => {
+                    if(event.key === 'Enter' && inputVal.trim() !== ''){
+                        setMessages(curr => [...curr, inputVal]);
+                        setInputVal('');
+                    }
+                    if(event.key === 'Delete')setMessages([]);
+                }}
+                />
+            </div>
         </div>
     )
 }
