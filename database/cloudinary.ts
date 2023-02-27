608829e3958e369c5c5cb045ec53bfb906745db8
p_tsanev <plamen_canev@abv.bg>
2023-02-27 19:04:48 +0200

    Added channel creation in atlas db and image saving to cloudinary

--- database/cloudinary.ts
+++ database/cloudinary.ts
@@ -1,10 +1,10 @@
-const cloudinary = require('cloudinary').v2;
+import cloudinary from 'cloudinary'
 
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: 'dh66s37ae',
     api_key: '271213257271594',
     api_secret: 'rKzYCCr0BEDQQYYiIYXpnC1W5Aw',
     secure: true
 });
 
-module.exports = cloudinary;
+export default cloudinary
--- pages/api/channel/addChannel.ts
+++ pages/api/channel/addChannel.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-const cloudinary = require('../../../database/cloudinary');
+
+import connectMongo from "@/database/connection";
+import Channel from "@/models/Channel";
+
+import cloudinary from "@/database/cloudinary";
 
 interface CloudinaryResponse {
     etag: string
@@ -7,16 +11,23 @@ interface CloudinaryResponse {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     return new Promise<void>(async resolve => {
+        await connectMongo();
         const {image, name} = req.body;
         //console.log(image, name);
 
-        // cloudinary.v2.uploader.upload(image, {use_filename: true, folder: 'channel-banners', responsive_breakpoints: { create_derived: true, bytes_step: 20000, min_width: 200, max_width: 1000 }})
-        // .then((response: CloudinaryResponse) => {
-        //     const cloudImage = response.etag;
-        //     console.log(cloudImage);
-        // })
+        cloudinary.v2.uploader.upload(image, {use_filename: true, folder: 'channel-banners', responsive_breakpoints: { create_derived: true, bytes_step: 20000, min_width: 200, max_width: 1000 }})
+        .then((response: CloudinaryResponse) => {
+            const cloudImage = response.etag;
+            console.log(cloudImage);
+
+            const channel = new Channel({
+                name: name,
+                icon: response.etag
+            })
+            channel.save();
+        })
 
         res.status(200).end();
-        resolve()
+        resolve();
     })
 }
