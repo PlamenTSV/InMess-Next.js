eca06457d6748cc3a846b8b524eec0b72fe3c4c4
p_tsanev <plamen_canev@abv.bg>
2023-03-24 17:57:59 +0200

    Changed global font to Open Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import './global.css';
 
+import { Open_Sans } from '@next/font/google';
+
+const opensans = Open_Sans({
+  weight: "300",
+  subsets: ['latin'],
+  display: 'swap',
+  preload: false
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={opensans.className}>
       <head />
       <body>{children}</body>
     </html>
