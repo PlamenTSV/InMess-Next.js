5181fe2511bbe6a7ee200d9d29d40b9391c4fd7c
p.tsanev <plamen_canev@abv.bg>
2023-04-11 19:20:55 +0300

    Login function optimization for Vercel

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -8,34 +8,36 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
     await connectMongo();
   
     const {username, password} = req.body;
 
-    try {
-      const users = await User.find({username: username});
-    
-      users.forEach(user => {
-        if(bcrypt.compareSync(password, user.password)){
-    
-          const sessionToken = jwt.sign({id: user.id}, process.env.NEXT_PUBLIC_SESSION_SECRET, { expiresIn: '1d' });
+    const user = await User.findOne({username: username});
 
-          res.setHeader('Set-Cookie', serialize('sessionToken', sessionToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'strict', 
-            maxAge: 60 * 60  * 24,
-            path: '/'
-          }));
-          
-        }
-      })
-      
-      res.status(200).send({message: 'Successful login!'});
-    } catch (error) {
-      console.log(error);
-      res.status(400).send({message: 'No such user found!'});
+    if (!user) {
+      return res.status(400).send({message: 'No such user found!'});
     }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({message: 'Invalid password!'});
+    }
+
+    const sessionToken = jwt.sign({id: user.id}, process.env.NEXT_PUBLIC_SESSION_SECRET, { expiresIn: '1d' });
+
+    res.setHeader('Set-Cookie', serialize('sessionToken', sessionToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict', 
+      maxAge: 60 * 60  * 24,
+      path: '/'
+    }));
+          
+    res.status(200).send({message: 'Successful login!'});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({message: 'Internal server error!'});
+  }
 }
 
 
