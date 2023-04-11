e10105f9bd107ef9b33be004fdf34bd556a281a0
p.tsanev <plamen_canev@abv.bg>
2023-04-11 16:50:20 +0300

    Quick fix to channel deletion functionality

--- pages/api/channel/delete.ts
+++ pages/api/channel/delete.ts
@@ -1,8 +1,12 @@
+import cloudinary from "@/database/cloudinary";
 import Channel from "@/models/Channel";
+import Message from "@/models/Message";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { channelID } = req.query;
+
+    const { icon: iconToDelete } = await Channel.findById({_id: channelID});
 
     try {
         const result = await Channel.deleteOne({_id: channelID});
@@ -11,6 +15,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             res.status(404).send({message: 'Channel not found'});
             return;
         }
+
+        cloudinary.v2.uploader.destroy('channel-banners/' + iconToDelete)
+        .then(() => console.log('Deleted channel with id ' + channelID))
+        .catch(error => console.error(error));
+
+        await Message.deleteMany({ channelID: channelID });
 
         res.status(200).end();
     } catch(error){
