6f7333c963042fc307a432f92ceed93fbef58788
p.tsanev <plamen_canev@abv.bg>
2023-04-12 22:01:24 +0300

    Experimental compression for Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
     appDir: true,
+  },
+  compress: true,
+  // Enable Brotli compression
+  webpack(config) {
+    config.plugins.push(new CompressionPlugin({
+      algorithm: 'brotliCompress',
+      test: /\.(js|svg)$/,
+      threshold: 15240,
+      minRatio: 0.8,
+    }))
+    return config
   },
 }
 
